9c8245e2d9e533973081b8fbcd76a301db6535fb
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-25 16:12:38 +0800

    Add HackerNews page fetch

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
+import Link from 'next/link';
 import Layout from '../components/Layout';
 import StoryList from '../components/StoryList';
 
-const Index = ({ stories }) => {
+const Index = ({ stories, page }) => {
   if (stories.length === 0) {
     return <Error statusCode={503} />;
   }
@@ -13,21 +14,53 @@ const Index = ({ stories }) => {
       description="A HackerNews clone built using NextJS"
     >
       <StoryList stories={stories} />
+      <footer>
+        {page > 1 && (
+          <Link href={`/?page=${page - 1}`}>
+            <a>Previous Page</a>
+          </Link>
+        )
+      }
+        <Link href={`/?page=${page + 1}`}>
+          <a>
+            Next Page
+            {' '}
+            {page + 1}
+          </a>
+        </Link>
+      </footer>
+      <style jsx>
+        {`
+        footer {
+          padding: 1em;
+        }
+        footer > * {
+          margin-right: 1rem;
+        }
+        footer a {
+          font-weight: bold;
+          color: black;
+          text-decoration: none;
+        }
+        footer a:hover {
+          text-decoration: underline;
+        }
+      `}
+      </style>
     </Layout>
   );
 };
 
-Index.getInitialProps = async () => {
+Index.getInitialProps = async ({ query }) => {
   let stories;
-  const res = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+  const page = Number(query.page) || 1;
+  const res = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
   try {
     stories = await res.json();
-    return { stories };
   } catch (err) {
-    console.log('Error: ', err);
     stories = [];
   }
-  return { stories };
+  return { stories, page };
 };
 
 export default Index;
